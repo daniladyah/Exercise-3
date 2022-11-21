87cc2e5d6fdacd4021158fbd863b4d23660baf7f
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-11-21 16:09:33 +0700

    menambahkan listEmpty

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -22,28 +22,45 @@ namespace Exercise_3
             LAST = null;
         }
 
-        public bool Search(int rollNo, ref Node previous, ref Node current)/
-            /*Searches for the specified node*/
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    internal class Program
-    {
-        static void Main(string[] args)
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*Searches for the specified node*/
         {
+            for (previous = current = LAST.next; current != LAST; previous =
+             current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*If the node is present at the end*/
+                return true;
+            else
+                return false;/*returns false if the node is not found*/
+        }
+        public bool listEmpty()
+        {
+            if (LAST == null)
+                return true;
+            else
+                return false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            }
         }
     }
 }
