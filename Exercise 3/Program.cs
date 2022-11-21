26846985bf11250f2ee65c0d452a9d98e7db9a32
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-11-21 15:39:03 +0700

    Add project files.

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
